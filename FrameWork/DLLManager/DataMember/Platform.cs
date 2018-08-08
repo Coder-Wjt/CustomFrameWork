ba3eae004eeb29d686ad61e5fb941c9f3546b481
Coder-Wjt <33034805+Coder-Wjt@users.noreply.github.com>
2018-08-08 17:34:43 +0800

    create

---
+++ FrameWork/DLLManager/DataMember/Platform.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DLLManager.DataMember
+{
+    /// <summary>
+    /// 平台标识枚举
+    /// </summary>
+    public enum Platform
+    {
+        /// <summary>
+        /// Windows平台
+        /// </summary>
+        Windows,
+        /// <summary>
+        /// Android_Linux平台
+        /// </summary>
+        Android_Linux,
+        /// <summary>
+        /// IOS平台
+        /// </summary>
+        IOS,
+    }
+}
